8471af574c130cb14fb9e5d760e358a872743923
Felps <jfelipesmarques@gmail.com>
2026-03-05 17:27:11 -0300

    O Tatame Finalizado

---
+++ O Tatame (Manipulação Básica)/script.js
@@ -0,0 +1,4 @@
+const newFighter = document.getElementById('perfil-lutador');
+// newFighter.innerHTML = `<h2> Lutador: Bolinha </h2>
+// <p> <b>Faixa:</b> Branca </p>`
+newFighter.innerHTML = "<h2> Lutador: Bolinha </h2>" + "<p> <b>Faixa:</b> Branca </p>";
