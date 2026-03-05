bbe4031255b60a914b7b72f1d8d2e1c00e49aca3
Felps <jfelipesmarques@gmail.com>
2026-03-05 17:41:36 -0300

    testei captando elemento por elemento

--- O Salão Vip (Dark Mode)/script.js
+++ O Salão Vip (Dark Mode)/script.js
@@ -1,6 +1,17 @@
+// - mudar nome do botao
+// - mudar cor do botao
+// - mudar cor do background geral
+// - mudar intro 
+// - mudar benefits
+// - reverter para o original
+
 const btn = document.querySelector('button')
+const p = document.querySelector('.intro p') ;
 
 btn.addEventListener('click', () => {
-    document.body.style.backgroundColor = 'lightblue';
-    btn.innerHTML = 'Cliquei';
+    // document.body.style.backgroundColor = 'lightblue';
+    // btn.innerHTML = 'Cliquei';
+    document.body.classList.toggle('modo-vip')
+    btn.innerHTML = 'Normal Mode'
+    p.classList.toggle('modo-vip')
 })
